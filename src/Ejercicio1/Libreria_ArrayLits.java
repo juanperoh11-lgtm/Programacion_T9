5f177d2b38489eeefa4e76be26ebdef6c2e21622
juanperoh11-lgtm. <juanperoh11@gmail.com>
2026-03-25 18:04:26 +0100

    Ejercicio 1

--- src/Ejercicio1/Libreria_ArrayLits.java
+++ src/Ejercicio1/Libreria_ArrayLits.java
@@ -2,69 +2,61 @@ package Ejercicio1;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class Libreria_ArrayLits {
 	
-	//Apartado a)
 	public static ArrayList<Integer> generaArrayListInt(int n, int min, int max) {
-		ArrayList<Integer> lista = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			int aleatorio = (int) (Math.random() * (max - min + 1)) + min;
-			lista.add(aleatorio);
-		}
-		return lista;
-	}
+        Random rd = new Random();
+        ArrayList<Integer> lista = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            lista.add(rd.nextInt(min, max + 1));
+        }
+        return lista;
+    }
 
-	//Apartado b)
-	public static int minimoArrayListInt(ArrayList<Integer> lista) {
-		if (lista.isEmpty())
-			return Integer.MAX_VALUE;
-		int min = lista.get(0);
-		for (int num : lista) {
-			if (num < min)
-				min = num;
-		}
-		return min;
-	}
+    // Apartado b)
+    public static int minimoArrayListInt(ArrayList<Integer> lista) {
+        int min = Integer.MAX_VALUE;
+        for (int num : lista) {
+            if (num < min) min = num;
+        }
+        return min;
+    }
 
-	//Apartado c)
-	public static int maximoArrayListInt(ArrayList<Integer> lista) {
-		if (lista.isEmpty())
-			return Integer.MAX_VALUE;
-		int max = lista.get(0);
-		for (int num : lista) {
-			if (num > max)
-				max = num;
-		}
-		return max;
-	}
+    // Apartado c)  
+    public static int maximoArrayListInt(ArrayList<Integer> lista) {
+        int max = Integer.MIN_VALUE;  
+        for (int num : lista) {
+            if (num > max) max = num;
+        }
+        return max;
+    }
 
-	//Apartado d)
-	public static double mediaArrayListInt(ArrayList<Integer> lista) {
-		if (lista.isEmpty())
-			return 0;
-		int suma = 0;
+    // Apartado d)
+    public static double mediaArrayListInt(ArrayList<Integer> lista) {
+        if (lista.isEmpty()) return 0;
+        double suma = 0;
+        for (int a : lista) {
+            suma += a; 
+        }
+        return suma / lista.size();
+    }
 
-		for (int num : lista) {
-			suma += num;
-		}
-		return (double) suma / lista.size();
-	}
+    // Apartado e)
+    public static boolean estaEnArrayListInt(ArrayList<Integer> lista, int n) {
+        return lista.contains(n);
+    }
+    
+    // Apartado f)
+    public static int posicionEnArrayList(ArrayList<Integer> lista, int n) {
+        return lista.indexOf(n);
+    }
 
-	//Apartado e)
-	public static boolean estaEnArrayListInt(ArrayList<Integer> lista, int n) {
-		return lista.contains(n);
-	}
-	
-	//Apartado f)
-	public static int posicionEnArrayList(ArrayList<Integer> lista, int n) {
-		return lista.indexOf(n);
-	}
-
-	//Apartado g)
-	public static ArrayList<Integer> volteaArrayListInt(ArrayList<Integer> lista) {
-		ArrayList<Integer> listaVolteada = new ArrayList<>(lista);
-		Collections.reverse(listaVolteada);
-		return listaVolteada;
-	}
+    // Apartado g)  
+    public static ArrayList<Integer> volteaArrayListInt(ArrayList<Integer> lista) {
+        ArrayList<Integer> volteado = new ArrayList<>(lista);
+        Collections.reverse(volteado); 
+        return volteado;
+    }
 }
--- src/Ejercicio1/Main.java
+++ src/Ejercicio1/Main.java
@@ -5,23 +5,15 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		
-ArrayList<Integer> miLista = Libreria_ArrayLits.generaArrayListInt(10, 1, 50);
-        
-        System.out.println("Lista generada: " + miLista);
-        
-        // Probamos mínimo, máximo y media
-        System.out.println("Mínimo: " + Libreria_ArrayLits.minimoArrayListInt(miLista));
-        System.out.println("Máximo: " + Libreria_ArrayLits.maximoArrayListInt(miLista));
-        System.out.printf("Media: %.2f\n", Libreria_ArrayLits.mediaArrayListInt(miLista));
-        
-        // Probamos búsqueda
-        int buscar = miLista.get(0); // Buscamos el primero para asegurar que está
-        System.out.println("¿Está el número " + buscar + "? " + Libreria_ArrayLits.estaEnArrayListInt(miLista, buscar));
-        System.out.println("Posición del número " + buscar + ": " + Libreria_ArrayLits.posicionEnArrayList(miLista, buscar));
-        
-        // Probamos el volteo
-        ArrayList<Integer> invertida = Libreria_ArrayLits.volteaArrayListInt(miLista);
-        System.out.println("Lista volteada: " + invertida);
-    }
+
+		ArrayList<Integer> miLista = Libreria_ArrayLits.generaArrayListInt(10, 20, 30);
+
+		System.out.println("Lista generada: " + miLista);
+		System.out.println("Mínimo: " + Libreria_ArrayLits.minimoArrayListInt(miLista));
+		System.out.println("Máximo: " + Libreria_ArrayLits.maximoArrayListInt(miLista));
+		System.out.printf("Media: %.2f\n", Libreria_ArrayLits.mediaArrayListInt(miLista));
+		System.out.println("El número 14 esta ?" + Libreria_ArrayLits.estaEnArrayListInt(miLista, 14));
+		System.out.println("El número 14 esta ?" + Libreria_ArrayLits.posicionEnArrayList(miLista, 14));
+		System.out.println("Array volteado: " +  Libreria_ArrayLits.volteaArrayListInt(miLista)) ;
+	}
 }

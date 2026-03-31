aafcd7fee3482c79e847d5e0855731bb1e78ef56
juanperoh11-lgtm. <juanperoh11@gmail.com>
2026-03-31 14:28:27 +0200

    Ejercicio 2

---
+++ src/Ejercicio2/Main.java
@@ -0,0 +1,50 @@
+package Ejercicio2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		ArrayList<Double> alturas = new ArrayList<>();
+		double suma = 0;
+
+		System.out.println("Introduce la altura en metros, pulsa 0 para salir");
+		while (true) {
+			double h = sc.nextDouble();
+			if (h == 0)
+				break;
+			alturas.add(h);
+			suma += h;
+		}
+
+		if (alturas.isEmpty()) {
+			System.out.println("No se introdujeron alturas.");
+			return;
+		}
+
+		double media = suma / alturas.size();
+		double max = alturas.get(0);
+		double min = alturas.get(0);
+		int porEncima = 0;
+		int porDebajo = 0;
+
+		for (double h : alturas) {
+			if (h > max)
+				max = h;
+			if (h < min)
+				min = h;
+
+			if (h > media) {
+				porEncima++;
+			} else if (h < media) {
+				porDebajo++;
+			}
+		}
+
+	}
+
+}

57b961310b6f014fbbe0cf77a92223491ebe0936
juanperoh11-lgtm. <juanperoh11@gmail.com>
2026-04-05 08:57:42 +0200

    Ejercicio 6

---
+++ src/Ejercicio6/Main.java
@@ -0,0 +1,22 @@
+package Ejercicio6;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> Secuencia = new ArrayList<Integer>();
+		final int n = 10;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j <= i; j++) {
+				Secuencia.add(i);
+			}
+		}
+
+		System.out.println("Secuencia generada: " + Secuencia);
+
+	}
+
+}

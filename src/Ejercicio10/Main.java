e859385b6550609197f8abf9fbf5868e5973226a
juanperoh11-lgtm. <juanperoh11@gmail.com>
2026-04-07 19:00:36 +0200

    Ejercicio 10(Corregido)

--- src/Ejercicio10/Main.java
+++ src/Ejercicio10/Main.java
@@ -5,73 +5,94 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ArrayList<Double> num = new ArrayList<Double>();
-		Scanner sc = new Scanner(System.in);
-		double a;
+        ArrayList<Double> num = new ArrayList<Double>();
+        Scanner sc = new Scanner(System.in);
+        double a;
+        char opcion;
 
-		System.out.println("---Menu---");
-		System.out.println("a)");
-		System.out.println("b)");
-		System.out.println("c)");
-		System.out.println("d)");
-		System.out.println("e)");
-		System.out.println("f)");
-		char opcion = sc.next().toLowerCase().charAt(0);
-		do {
+        do {
+            System.out.println("\n--- Menu ---");
+            System.out.println("a) Agregar número");
+            System.out.println("b) Buscar número");
+            System.out.println("c) Modificar número ( ");
+            System.out.println("d) Eliminar número");
+            System.out.println("e) Insertar en posición específica  )");
+            System.out.println("f) Salir");
+            System.out.print("Seleccione una opción: ");
+            
+            opcion = sc.next().toLowerCase().charAt(0);
 
-			System.out.println("---Menu---");
-			System.out.println("a)");
-			System.out.println("b)");
-			System.out.println("c)");
-			System.out.println("d)");
-			System.out.println("e)");
-			System.out.println("f)");
-			switch (opcion) {
-			case 'a':
-				System.out.println("Introduce un número para agregrar al array");
-				a = sc.nextDouble();
-				num.add(a);
-				System.out.println(num);
-				break;
+            switch (opcion) {
+                case 'a':
+                    System.out.print("Introduce un número: ");
+                    a = sc.nextDouble();
+                    
+                    num.add(a);
+                    System.out.println("Lista actual: " + num);
+                    break;
 
-			case 'b':
-				System.out.println("Introduce un número a buscar");
-				a = sc.nextDouble();
-				int posicion = num.indexOf(a);
+                case 'b':
+                    System.out.print("Número a buscar: ");
+                    a = sc.nextDouble();
+                    int posicion = num.indexOf(a);
+                    
+                    if (posicion != -1)
+                        System.out.println("Encontrado en la posición: " + posicion);
+                    else
+                        System.out.println("El número no está en la lista.");
+                    break;
 
-				if (posicion != -1)
-					System.out.println("El número " + a + " se encuentra en la posición: " + posicion);
-				else
-					System.out.println("El número no se encuentra dentro del array");
-				break;
+                case 'c':
+                    if (num.isEmpty()) {
+                        System.out.println("La lista está vacía.");
+                    } else {
+                        System.out.print("Nuevo número: ");
+                        a = sc.nextDouble();
+                        
+                        System.out.print("Posición a modificar: ");
+                        int posMod = sc.nextInt();
+                        
+                        if (posMod >= 0 && posMod < num.size()) {
+                            num.set(posMod, a);
+                        } else {
+                            System.out.println("Posición inválida.");
+                        }
+                    }
+                    break;
 
-			case 'c':
-				System.out.println("Introducce un nuevo número y la posicón en la que lo quieres colocar");
-				a = sc.nextDouble();
-				posicion = sc.nextInt();
-				num.set(posicion, a);
-				break;
+                case 'd':
+                    System.out.print("Número a eliminar: ");
+                    a = sc.nextDouble();
+                    if (num.remove(Double.valueOf(a))) {
+                        System.out.println("Numero eleminado");
+                    } else {
+                        System.out.println("No se encontró el número.");
+                    }
+                    break;
 
-			case 'd':
-				System.out.println("Introduce el elemento a eliminar del ArrayList");
-				a = sc.nextDouble();
-				num.remove(a);
-				break;
+                case 'e':
+                    System.out.print("Número a añadir: ");
+                    a = sc.nextDouble();
+                    System.out.print("Posición: ");
+                    int posAdd = sc.nextInt();
+                    if (posAdd >= 0 && posAdd <= num.size()) {
+                        num.add(posAdd, a);
+                    } else {
+                        System.out.println("Posición inválida.");
+                    }
+                    break;
 
-			case 'e':
-				System.out.println("Introduce el elemtno que quieres añadir y la posicón en la que lo quieras colocar");
-				a = sc.nextDouble();
-				posicion = sc.nextInt();
-				num.add(posicion, a);
-				break;
+                case 'f':
+                    System.out.println("Saliendo del programa...");
+                    break;
+                
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcion != 'f');
 
-			case 'f':
-				System.out.println("Saliendo del programa");
-				break;
-			}
-		} while (opcion != 'f');
-		sc.close();
-	}
+        sc.close();
+    }
 }
